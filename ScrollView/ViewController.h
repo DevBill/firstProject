bbc7893a72eb03b4dd314663f7b51fa621785291
DevBill <joe_wang113@163.com>
2016-03-02 11:36:44 +0800

    Initial Commit

---
+++ ScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView
+//
+//  Created by 王备 on 16/2/24.
+//  Copyright © 2016年 wangbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollView/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  ScrollView
+//
+//  Created by 王备 on 16/2/24.
+//  Copyright © 2016年 wangbei. All rights reserved.
+//
+
+
+#import "ViewController.h"
+
+#define MVIEWFRAME self.view.frame.size.width
+
+@interface ViewController ()
+
+@property(nonatomic,assign)NSInteger tabCount;
+@property(nonatomic,strong)UIScrollView * topScrollview;
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+   
+    NSArray * array =@[@"界面1",@"界面2",@"界面3",@"界面4",@"界面5",@"界面6",@"界面7"];
+
+    self.topScrollview = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    
+    self.tabCount = array.count;
+    
+    CGFloat width = MVIEWFRAME/6;
+    
+    if (self.tabCount !=6) {
+        
+        _topScrollview.contentSize = CGSizeMake(width * _tabCount, 50);
+    }else{
+        
+        
+        _topScrollview.contentSize = CGSizeMake(MVIEWFRAME,50);
+        
+    }
+    
+    [self.view addSubview:_topScrollview];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
